4c5f48650246ff631565b10cd118e42c48866187
Andy Armstrong <andy@hexten.net>
2011-02-12 04:04:36 +0000

    Observe rsync exit codes

--- post-backup-hook.sh
+++ post-backup-hook.sh
@@ -2,7 +2,9 @@
 
 /usr/sbin/service mysql-eric-slave stop
 $RSYNC -av /var/lib/mysql-eric-slave/* $WORK/var/lib/mysql
+RC=$?
 /usr/sbin/service mysql-eric-slave start
+after_rsync $RC
 
 # vim:ts=2:sw=2:sts=2:et:ft=sh
 
