52d673f914974c97b1f36e3a8a41c5bf57eeeb9e
Andy Armstrong <andy@hexten.net>
2011-02-17 12:11:08 +0000

    Get --link-dest right

--- post-backup-hook.sh
+++ post-backup-hook.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+MS_PATH="var/lib/mysql"
+MS_PATH_SLAVE="var/lib/mysql-eric-slave"
+
+RS_MS_OPT=""
+if [ -e "$CURRENT" ]; then
+  LATEST=$(readlink -f "$CURRENT")
+  RS_MS_OPT="$RS_MS_OPT --link-dest $LATEST/$MS_PATH"
+fi
+
 /usr/sbin/service mysql-eric-slave stop
-$RSYNC -av $RSYNC_EXTRA /var/lib/mysql-eric-slave/* $WORK/var/lib/mysql
+$RSYNC -av $RS_MS_OPT /$MS_PATH_SLAVE/ $WORK/$MS_PATH
 RC=$?
 /usr/sbin/service mysql-eric-slave start
 after_rsync $RC
